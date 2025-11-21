0834e369e9f83b7557a973c72917e8ada20967f4
shutu <3321652135@qq.com>
2025-11-21 11:03:42 +0800

    feat:增加消息处理异常处理

---
+++ devSphere-chat/src/main/java/com/shutu/devSphere/service/MessageErrorLogService.java
@@ -0,0 +1,10 @@
+package com.shutu.devSphere.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shutu.devSphere.model.entity.MessageErrorLog;
+
+/**
+ * 消息死信日志服务接口
+ */
+public interface MessageErrorLogService extends IService<MessageErrorLog> {
+}
