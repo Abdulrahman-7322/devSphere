e966dce898ecbd1657b35bc27c6a8391a0701a0f
shutu <3321652135@qq.com>
2025-11-24 21:15:58 +0800

    feat:新增用户个人中心

---
+++ devSphere-chat/src/main/java/com/shutu/devSphere/model/vo/moment/UserVo.java
@@ -0,0 +1,11 @@
+package com.shutu.devSphere.model.vo.moment;
+
+import lombok.Data;
+
+@Data
+public class UserVo {
+    private Long uid;
+    private String username;
+    private String avatar;
+    private String displayName;
+}
